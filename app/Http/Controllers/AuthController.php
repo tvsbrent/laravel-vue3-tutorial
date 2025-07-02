5c4f2e5581eabbc36f83a297560cc7d60898cd65
Brent Schmidt <brent.a.schmidt@gmail.com>
2025-07-02 12:36:06 +0000

    feat: basic auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function create()
+    {
+        return inertia('Auth/Login');
+    }
+
+    public function store(Request $request)
+    {
+        $authed = Auth::attempt($request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]), true);
+        if (!$authed) {
+            throw ValidationException::withMessages(['email' => 'Authentication failed']);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->intended('/listings');
+    }
+
+    public function destroy() {}
+}
---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit.prevent="login">
+    <div class="w-1/2 mx-auto">
+      <div>
+        <label for="email" class="label">Email</label>
+        <input id="email" type="text" class="input" v-model="form.email" />
+        <div class="input-error" v-if="form.errors.email">{{form.errors.email}}</div>
+      </div>
+      <div class="mt-4">
+        <label for="password" class="label">Password</label>
+        <input
+          id="password"
+          type="password"
+          class="input"
+          v-model="form.password" />
+        <div class="input-error" v-if="form.errors.password">{{ form.errors.password }}</div>
+      </div>
+      <div class="mt-4">
+        <button class="btn-primary w-full" type="submit">Login</button>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script setup lang="ts">
+  import { useForm } from '@inertiajs/vue3';
+  import { route } from 'ziggy-js';
+
+  const form = useForm({
+    email: null,
+    password: null,
+  });
+
+  const login = () => form.post(route('login.store'));
+</script>
